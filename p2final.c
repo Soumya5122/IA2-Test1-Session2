8439d65c78a11a1cc033c36f7aa32f18cf7820a8
Soumya Dhanappa <97490356+Soumya5122@users.noreply.github.com>
2022-03-24 10:07:00 +0000

    main

--- p2final.c
+++ p2final.c
@@ -1,38 +1,36 @@
 #include<stdio.h>
-int input_side()
+int input_side(int a,int b,int c)
 {
-  int a,b,c;
+  
   printf("enter the sides of the triangle :\n");
-  scanf("%d,%d,%d",&a,&b,&c);
+  scanf("%d%d%d",&a,&b,&c);
 }
 
 int check_scalence(int a,int b,int c)
 {
   
-  if a!=b && a!=c;
+  if (a!=b && a!=c);
   
   
-  else if b!=a && b!=c;
+  else if (b!=a && b!=c);
   
   
-  if c
-   {
-     return scalence;
-   }
+  if (c);
+   
+     return a;
+   
 }
 
 void output(int a,int b,int c,int scalence)
 {
-  printf("among of these any sides are not egual to eachother\n",a,b,c,*scalence);
+  printf("among of these any sides are not egual to eachother\n",a,b,c,scalence);
 }
 
 int main()
 {
-  intput_side(a,b,c);
-  input a();
-  input b();
-  input c();
-  result check_scalence side(a,b,c);
-  output(result);
+  int a,b,c,scalence;
+  input_side(a,b,c);
+   check_scalence (a,b,c);
+  output(a,b,c,scalence);
   return 0;
 }
