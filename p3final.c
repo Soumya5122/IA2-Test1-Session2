d0e968bdb88edfc4d6d65ec7bd982b5839223522
Soumya Dhanappa <97490356+Soumya5122@users.noreply.github.com>
2022-03-30 18:38:10 +0000

    assignment

--- p3final.c
+++ p3final.c
@@ -1,35 +1,33 @@
-#include <stdio.h>
-
-int input_number()
+#include<stdio.h>
+int input_side()
 {
-  int num;
-  printf("Enter the number:\n");
-  scanf("%d", &num);
-  return num;
+  int n;
+  printf("enter the number\n");
+  scanf("%d",&n);
+  return n;
 }
-
 int is_composite(int n)
 {
-  for (int i = 2; i <= n/2; i++)
-  {
-    if (n%i == 0)
-      return 1;
-  }
+  int i;
+  for(i=2;i<=n/2;i++)
+    {
+      if(n%i == 0)
+        return 1;
+    }
   return 0;
 }
-
-void output(int n, int isComposite)
+void output(int n,int is_composite)
 {
-  if (isComposite==1)
-    printf("%d is a composite number\n", n);
-  else 
-    printf("%d is not a composite number\n", n);
+  if(is_composite==0)
+  printf("%d is a composite number\n",n);
+  else
+    printf("%d is not a composite number",n);
 }
-
 int main()
 {
-  int num = input_number();
-  int res = is_composite(num);
-  output(num, res);
+  int n,a;
+  n=input_side();
+  a=is_composite(n);
+  output(n,a);
   return 0;
 }
